a23626842af8dcf1a264b044d76d481cb57fc07e
ulngollm <ulngollm@gmail.com>
2025-03-31 13:00:51 +0300

    close chat after timeout

--- state_manager.go
+++ state_manager.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	tele "gopkg.in/telebot.v4"
 )
 
-// StateManager manages the mapping of states to handlers.
 type StateManager struct {
 	stateHandlers map[string]tele.HandlerFunc
 }
 
-// NewStateManager initializes a new StateManager with state-handler mappings.
 func NewStateManager() *StateManager {
 	return &StateManager{
 		stateHandlers: map[string]tele.HandlerFunc{
@@ -25,39 +24,29 @@ func NewStateManager() *StateManager {
 	}
 }
 
-// GetHandler returns the handler for a given state.
 func (sm *StateManager) GetHandler(state string) tele.HandlerFunc {
 	return sm.stateHandlers[state]
 }
 
-// Handlers for each state
-
 func greetHandler(c tele.Context) error {
-	//todo remove. retrieve from c.Get("session")
-	session, err := sessionHandler.getSession(c.Chat().ID)
-	if err != nil {
-		return err
-	}
-	evt := eventGreet
-	if !session.FSM.Can(evt) {
+	session := c.Get("session").(*Session)
+	e := eventGreet
+	if !session.FSM.Can(e) {
 		if err := session.FSM.Event(context.Background(), eventClose); err != nil {
 			return err
 		}
 	}
-	if err := session.FSM.Event(context.Background(), evt); err != nil {
+	if err := session.FSM.Event(context.Background(), e); err != nil {
 		return err
 	}
 	return c.Send("Добрый день! О чем вы хотите узнать?")
 }
 
 func askHandler(c tele.Context) error {
-	session, err := sessionHandler.getSession(c.Chat().ID)
-	if err != nil {
-		return err
-	}
+	session := c.Get("session").(*Session)
 	evt := eventAskQuestion
 	if !session.FSM.Can(evt) {
-		return nil // No transition possible, return
+		return nil
 	}
 	if err := session.FSM.Event(context.Background(), evt); err != nil {
 		return err
@@ -79,12 +68,15 @@ func askFeedbackHandler(c tele.Context) error {
 	if err := session.FSM.Event(ctx, evt); err != nil {
 		return err
 	}
-	select {
-	case <-ctx.Done():
-		if err := session.FSM.Event(context.Background(), eventClose); err != nil {
-			return err
+	go func() {
+		select {
+		case <-time.After(time.Minute):
+			if err := session.FSM.Event(context.Background(), eventClose); err != nil {
+				// Log the error if needed
+			}
+			fmt.Println("чат закрыт после ожидания 1 мин")
 		}
-	}
+	}()
 	return c.Send("Напишите нам оценку пжаста")
 }
 
@@ -104,33 +96,21 @@ func getFeedbackHandler(c tele.Context) error {
 }
 
 func closeChatHandler(c tele.Context) error {
-	session, err := sessionHandler.getSession(c.Chat().ID)
-	if err != nil {
+	session := c.Get("session").(*Session)
+	e := eventClose
+	if !session.FSM.Can(e) {
+		return nil
+	}
+	if err := session.FSM.Event(context.Background(), e); err != nil {
 		return err
 	}
-	if !session.FSM.Can(eventClose) {
-		return nil // No transition possible, return
-	}
-	if err := session.FSM.Event(context.Background(), eventClose); err != nil {
-		return err
-	}
-
 	return c.Send("Чат закрыт. Вы можете задать свой вопрос снова")
 }
 
-// GetHandlerForCurrentState returns the handler for the current state of the session.
-func (sm *StateManager) GetHandlerForCurrentState(chatID int64) (tele.HandlerFunc, error) {
-	session, err := sessionHandler.getSession(chatID)
-	if err != nil {
-		return nil, err
-	}
-	currentState := session.getCurrentState()
+func (sm *StateManager) GetHandlerForCurrentState(currentState string) (tele.HandlerFunc, error) {
 	return sm.GetHandler(currentState), nil
 }
 
-// Define getCurrentState method for Session
-
-// getCurrentState returns the current state of the session.
 func (s *Session) getCurrentState() string {
 	return s.FSM.Current()
 }
